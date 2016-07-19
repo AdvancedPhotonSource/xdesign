5f56ddc6cf9aab9604ccb54fb14fc2e463ca73c0
dgursoy <sparsedata@gmail.com>
2016-07-19 15:26:06 -0500

    probe definition added

--- phantom/geometry.py
+++ phantom/geometry.py
@@ -67,7 +67,9 @@ __all__ = ['Point',
            'scale',
            'segment',
            'beamcirc',
-           'Registry']
+           'Phantom',
+           'Probe',
+           'sinogram']
 
 
 class Point(object):
@@ -96,9 +98,21 @@ class Point(object):
         """Addition."""
         return Point(self.x + other.x, self.y + other.y)
 
-    def __rmul__(self, c):
+    def __mul__(self, c):
         """Scalar multiplication."""
         return Point(c * self.x, c * self.y)
+
+    def distance(self, point):
+        """Returns the distance from a point."""
+        dx = self.x - point.x
+        dy = self.y - point.y
+        return np.sqrt(dx * dx + dy * dy)
+
+    def rotate(self, theta):
+        """Returns a point rotated around origin."""
+        px = self.x * np.cos(theta) - self.y * np.sin(theta)
+        py = self.x * np.sin(theta) + self.y * np.cos(theta)
+        return Point(px, py)
 
     def list(self):
         """Returns the point's list representation."""
@@ -146,24 +160,47 @@ class Line(object):
     """
 
     def __init__(self, p1, p2):
+        if p1 == p2:
+            raise ValueError('Requires two unique points.')
         self.p1 = p1
         self.p2 = p2
 
-        if self.p1 == self.p2:
-            raise ValueError('Requires two unique points.')
-
-        elif self.p1.x == self.p2.x:
-            self.slope = np.inf
-            self.intercept = 0.
-            self.vertical = True
-
+    @property
+    def slope(self):
+        if self.vertical:
+            return np.inf
         else:
-            self.slope = (self.p2.y - self.p1.y) / (self.p2.x - self.p1.x)
-            self.intercept = self.p1.y - self.slope * self.p1.x
-            self.vertical = False
+            return (self.p2.y - self.p1.y) / (self.p2.x - self.p1.x)
+
+    @property
+    def intercept(self):
+        if self.vertical:
+            return 0.
+        else:
+            return self.p1.y - self.slope * self.p1.x
+
+    @property
+    def vertical(self):
+        if self.p1.x == self.p2.x:
+            return True
+        else:
+            return False
+
+    @property
+    def horizontal(self):
+        if self.p1.y == self.p2.y:
+            return True
+        else:
+            return False
 
     def __str__(self):
         return "%s, %s, %s" % (self.p1, self.p2)
+
+    def normal(self):
+        length = self.p1.distance(self.p2)
+        dx = (self.p1.x - self.p2.x) / length
+        dy = (self.p1.y - self.p2.y) / length
+        return Point(-dy, dx)
 
     def equation(self):
         if self.vertical:
@@ -180,13 +217,13 @@ class Beam(Line):
     ----------
     p1 : Point
     p2 : Point
-    width : scalar, optional
-        Width of the beam.
+    size : scalar, optional
+        Size of the beam.
     """
 
-    def __init__(self, p1, p2, width=0):
+    def __init__(self, p1, p2, size=0):
         super(Beam, self).__init__(p1, p2)
-        self.width = float(width)
+        self.size = float(size)
 
     def __str__(self):
         return super(Beam, self).__str__()
@@ -282,20 +319,20 @@ def beamcirc(beam, circle):
     """
 
     # Passive coordinate transformation.
-    _circle = rotate(
+    _center = rotate(
         point=circle.center,
         theta=-np.arctan(beam.slope),
         origin=Point(0, beam.intercept))
 
-    # Correction if line is parallel to x-axis.
-    if beam.p1.y == beam.p2.y:
-        dy = beam.p1.y
+    # Correction if line is vertical to x-axis.
+    if beam.vertical:
+        dy = beam.p1.x
     else:
-        dy = 0
+        dy = -beam.intercept
 
     # Calculate the area deending on how the beam intersects the circle.
-    p1 = _circle.y - beam.width / 2. + dy
-    p2 = _circle.y + beam.width / 2. + dy
+    p1 = _center.y - beam.size / 2. + dy
+    p2 = _center.y + beam.size / 2. + dy
     pmin = min(abs(p1), abs(p2))
     pmax = max(abs(p1), abs(p2))
     if pmin < circle.radius:
@@ -304,7 +341,7 @@ def beamcirc(beam, circle):
                 area = segment(circle, pmin)
             else:
                 area = circle.area() - segment(circle, pmin)
-        elif pmax < circle.radius:  #
+        elif pmax < circle.radius:
             area = abs(segment(circle, p1) - segment(circle, p2))
     elif p1 * p2 < 0:
         area = circle.area()
@@ -313,11 +350,13 @@ def beamcirc(beam, circle):
     return area
 
 
-class Registry():
-    """Bookkeeping of circles generated.
+class Phantom():
+    """Phantom generation class.
 
     Attributes
     ----------
+    shape : string
+        Shape of the phantom. Available options: circle, square.
     population : scalar
         Number of generated circles in the phantom.
     density : scalar
@@ -327,12 +366,14 @@ class Registry():
     """
 
     def __init__(self):
+        self.shape = 'circle'
         self.population = 0
         self.density = 0
         self.feature = []
 
-    def __str__(self):
-        return "%s" % self.feature
+    def list(self):
+        for m in range(self.population):
+            print ("%s: %s" % (m, self.feature[m]))
 
     def add(self, circle):
         """Add a circle to the phantom.
@@ -341,14 +382,123 @@ class Registry():
         ----------
         circle : Circle
         """
-        circ = Circle(center, radius)
         self.feature.append(circle)
         self.density += circle.area()
         self.population += 1
 
     def remove(self):
-        """Remove last added circle from the phantom.
-        """
+        """Remove last added circle from the phantom."""
         self.population -= 1
         self.density -= self.feature[-1].area()
         self.feature.pop()
+
+    def generate_random_point(self, margin=0):
+        """Generates a random point in the phantom.
+
+        Parameters
+        ----------
+        margin : scalar
+            Determines the margin value of the shape.
+            Points will not be created in the margin area.
+
+        Returns
+        -------
+        Point
+            Random point.
+        """
+        if self.shape == 'square':
+            x = np.random.uniform(margin, 1 - margin)
+            y = np.random.uniform(margin, 1 - margin)
+        elif self.shape == 'circle':
+            r = np.random.uniform(0, 0.5 - margin)
+            a = np.random.uniform(0, 2 * np.pi)
+            x = r * np.cos(a) + 0.5
+            y = r * np.sin(a) + 0.5
+        return Point(x, y)
+
+    def sprinkle(self, counts, radius, gap=0, collision=False):
+        """Sprinkles a number of circles.
+
+        Parameters
+        ----------
+        counts : int
+            Number of circles to be added.
+        gap : float, optional
+            Minimum gap between the circle boundaries.
+        collision : bool, optional
+            False if circles will be non overlapping.
+        """
+        for m in range(int(counts)):
+            center = self.generate_random_point(radius)
+            circle = Circle(center, radius + gap)
+            if not self.collision(circle) or collision:
+                self.add(Circle(center, radius))
+
+    def collision(self, circle):
+        """Checks if a circle is collided with others."""
+        for m in range(self.population):
+            dx = self.feature[m].center.x - circle.center.x
+            dy = self.feature[m].center.y - circle.center.y
+            dr = self.feature[m].radius + circle.radius
+            if np.sqrt(dx**2 + dy**2) < dr:
+                return True
+        return False
+
+    def plot(self):
+        """Plots the generated circles."""
+        import matplotlib.pyplot as plt
+        import matplotlib.patches as patches
+
+        f = plt.figure(figsize=(8, 8), facecolor='w')
+        a = f.add_subplot(111, aspect='equal')
+        for m in range(self.population):
+            a.add_patch(
+                patches.Circle(
+                    (self.feature[m].center.x,
+                     self.feature[m].center.y),
+                    self.feature[m].radius,
+                    transform=a.transAxes))
+        plt.grid('on')
+        plt.show()
+
+    def save(self, filename):
+        pass
+
+
+class Probe(Beam):
+
+    def __init__(self, p1, p2, size=0):
+        super(Probe, self).__init__(p1, p2, size)
+
+    def translate(self, dx):
+        """Translates beam along its normal direction."""
+        vec = self.normal() * dx
+        self.p1 += vec
+        self.p2 += vec
+
+    def rotate(self, theta, origin):
+        """Rotates beam around a given point."""
+        self.p1 = rotate(self.p1, theta, origin)
+        self.p2 = rotate(self.p2, theta, origin)
+
+    def measure(self, phantom):
+        newdata = 0
+        for m in range(phantom.population):
+            newdata += beamcirc(self, phantom.feature[m])
+        return newdata
+
+
+def sinogram(sx, sy, phantom):
+    """Generates sinogram given a phantom."""
+    size = 1. / sy
+    beta = np.pi / sx
+    sino = np.zeros((sx, sy))
+    p = Probe(Point(size / 2., 0), Point(size / 2., 1), size)
+    for m in range(sx):
+        print (m, m * beta * 180. / np.pi)
+        for n in range(sy):
+            sino[m, n] = p.measure(phantom)
+            p.translate(size)
+        p.translate(-1)
+        p.rotate(beta, Point(0.5, 0.5))
+    return sino
