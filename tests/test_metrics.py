95918b925de0239cd2b55577820968afa6bd2f16
Daniel Ching <daniel.j.ching@gmail.com>
2016-07-22 18:09:59 -0500

    Summary graphics for multiscale quality measurement

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,5 @@
 from phantom.plot import plot_metrics
-from phantom.metrics import _compute_ssim, compute_quality, ImageQuality
+from phantom.quality import _compute_ssim, compute_quality, ImageQuality
 from numpy.testing import assert_allclose, assert_raises, assert_equal
 import numpy as np
 import scipy
@@ -8,7 +8,7 @@ import scipy
 # SSIM metric
 
 def test_SSIM_same_image_is_unity():
-    img1 = scipy.ndimage.imread("tests/cameraman.bmp",flatten=True)
+    img1 = scipy.ndimage.imread("tests/cameraman.png")
     IQ = ImageQuality(img1,img1)
     IQ = _compute_ssim(IQ)
     assert_equal(IQ.qualities[0],1,err_msg="Mean SSIM is not unity.")
@@ -16,9 +16,11 @@ def test_SSIM_same_image_is_unity():
     assert_equal(img1.shape,IQ.maps[0].shape,err_msg="SSIMs map not the same size as input")
 
 def test_compute_quality_cameraman():
-    img1 = scipy.ndimage.imread("tests/cameraman.png")
-    img2 = scipy.ndimage.imread("tests/cameraman_SP.png")
-    img3 = scipy.ndimage.imread("tests/cameraman_H.png")
-    metrics = compute_quality(img1,[img2,img3])
+    img1 = scipy.ndimage.imread("tests/cameraman.png") # original
+    img2 = scipy.ndimage.imread("tests/cameraman_SP.png") # salt and pepper
+    img3 = scipy.ndimage.imread("tests/cameraman_H.png") # half mising
+    img4 = scipy.ndimage.imread("tests/cameraman_mixed.png")
+    # salt and pepper, gaussian noise, square? smoothing filter, guassian filter
+    metrics = compute_quality(img1,[img2,img3,img4],method="MSSSIM")
 
     plot_metrics(metrics)
