0d77dfbd95d799dace114266d242b6a8727e7d6b
Daniel Ching <daniel.j.ching@gmail.com>
2016-09-13 15:37:11 -0500

    Renamed CurvedEntity class to Curve.

--- xdesign/geometry.py
+++ xdesign/geometry.py
@@ -417,7 +417,7 @@ class Segment(LinearEntity):
         return Point.midpoint(self.p1, self.p2)
 
 
-class CurvedEntity(Entity):
+class Curve(Entity):
     """Base class for entities whose surface can be defined by a continuous
     equation.
 
@@ -427,7 +427,7 @@ class CurvedEntity(Entity):
     """
 
     def __init__(self, center):
-        super(CurvedEntity, self).__init__()
+        super(Curve, self).__init__()
         self.center = center
 
     @property
@@ -458,7 +458,7 @@ class CurvedEntity(Entity):
         raise NotImplementedError
 
 
-class Superellipse(CurvedEntity):
+class Superellipse(Curve):
     """Superellipse in 2-D cartesian space.
 
     Attributes
