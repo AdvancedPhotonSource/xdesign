02f9083a0fa372cdcbcc2c630a5a0b0da1a81101
Ming <mingdu2015@u.northwestern.edu>
2016-11-04 14:20:06 -0500

    rm demo in /xdesign

--- xdesign/util.py
+++ xdesign/util.py
@@ -3,17 +3,8 @@ import numpy as np
 
 def gen_mesh(max, shape):
     """Generate mesh grid.
-
-    Parameters:
-    -----------
-    lengths : ndarray
-        Half-lengths of axes in nm or nm^-1.
-    shape : ndarray
-        Number of pixels in each dimension.
     """
-    dim = len(max)
-    axes = [np.array([np.linspace(-max[0], max[0], shape[0])])]
-    for i in range(1, dim):
-        axes.append(np.linspace(-max[i], max[i], shape[i]))
-    res = np.meshgrid(*axes)
+    yy = np.linspace(-max[0], max[0], shape[0])
+    xx = np.linspace(-max[1], max[1], shape[1])
+    res = np.meshgrid(xx, yy)
     return res
