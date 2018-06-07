9c7f4306c04c1fcc0b3a731e4f6e32ae592567b7
Daniel Ching <carterbox@users.noreply.github.com>
2018-06-07 10:49:07 -0500

    API: Simplify Probe class
    Instead of using Probe as an object that it moved around for each
    measurement, it has been simplified to recieve all measurement
    positions at once. Using three arrays theta, h, v all measurement
    positions are calculated in bulk. This is faster than iterating
    through tranformations in a for loop.

--- tests/test_acquisition.py
+++ tests/test_acquisition.py
@@ -2,40 +2,32 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+import logging
 import os.path
 from numpy.testing import assert_allclose
 
 from xdesign.acquisition import *
-from xdesign.phantom import XDesignDefault
-
-from multiprocessing import Pool
+from xdesign.phantom import *
+from xdesign.plot import *
 import matplotlib.pyplot as plt
 
-
-def test_raster_scan():
-    positions = [p.numpy.flatten() for p in raster_scan(2, 2)]
-    positions = np.hstack(np.hstack(positions))
-
-    correct = np.array([0.25, -10, 0.25, 10,
-                        0.75, -10, 0.75, 10,
-                        0.75,  11, 0.75, -9,
-                        0.25,  11, 0.25, -9])
-
-    assert_allclose(positions, correct)
+SIZE = 64
 
 
 def test_sinogram():
-    p = XDesignDefault()
+    theta, h, v = raster_scan2D(SIZE, SIZE)
+    prb = Probe(size=1/SIZE)
+    phan = XDesignDefault()
+    phan.translate([-.5, -.5])
+    sidebyside(phan, SIZE)
 
-    pool = Pool()
-    sino, _ = sinogram(32, 32, p, pool=pool)
-    pool.close()
+    sino = prb.measure(phan, theta, h, v)
+    sino = -np.log(sino)
 
-    ref_file = 'tests/test_sinogram.npy'
+    plt.figure()
+    plt.imshow(sino.reshape(SIZE, SIZE), origin='lower')
 
-    # plt.imshow(sino)
-    # plt.show(block=True)
-
+    # ref_file = 'tests/test_sinogram.npy'
     # if not os.path.isfile(ref_file):
     #     ImportError('sinogram reference not found; use test_sinogram.ipynb' +
     #                 'to generate it')
@@ -43,3 +35,10 @@ def test_sinogram():
     # sino_reference = np.load(ref_file)
     #
     # assert_allclose(sino, sino_reference, atol=1e-2)
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    test_sinogram()
+    plt.show()
--- xdesign/acquisition.py
+++ xdesign/acquisition.py
@@ -62,6 +62,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+from xdesign.constants import RADIUS, DEFAULT_ENERGY
 from xdesign.geometry import *
 from xdesign.geometry import halfspacecirc
 import logging
@@ -80,14 +81,12 @@ __author__ = "Doga Gursoy"
 __copyright__ = "Copyright (c) 2016, UChicago Argonne, LLC."
 __docformat__ = 'restructuredtext en'
 __all__ = ['Probe',
-           'calculate_gram',
-           'sinogram',
-           'raster_scan',
+           'raster_scan2D',
            'raster_scan3D']
 
 
 class Probe(Line):
-    """An x-ray beam for probing Phantoms.
+    """A square cross-section x-ray beam for probing Phantoms.
 
     A Probe is initialized by two points and a size (diameter).
 
@@ -108,26 +107,15 @@ class Probe(Line):
         etc.
 
     """
-    def __init__(self, p1, p2, size=0.0, intensity=1.0, energy=15.0,
-                 circleapprox=32):
+    def __init__(self, p1=None, p2=None, size=0.0, intensity=1.0,
+                 energy=DEFAULT_ENERGY):
+        if p1 is None or p2 is None:
+            p1 = Point([RADIUS, 0])
+            p2 = Point([-RADIUS, 0])
         super(Probe, self).__init__(p1, p2)
         self.size = size
         self.intensity = intensity
         self.energy = energy
-        self.history = list()
-        # determine the length, position, and shape of the beam
-        radius = self.size / 2
-        half_length = self.p1.distance(self.p2) / 2
-        center = ((self.p2 + self.p1) / 2)._x
-        # make a bounding box around axis 0
-        hi = np.full(self.dim, radius)
-        lo = -hi
-        hi[0] = +half_length
-        lo[0] = -half_length
-        # find the vector which bisects the angle between [1,0,0] and the beam
-        u = Point([1] + [0]*(self.dim - 1))
-        v = self.p2 - self.p1
-        w = u.norm * v._x + v.norm * u._x
 
     def __repr__(self):
         return "Probe({}, {}, size={}, intensity={}, energy={})".format(
@@ -138,41 +126,12 @@ class Probe(Line):
         """Return the string respresentation of the Beam."""
         return "Probe(" + super(Probe, self).__str__() + ")"
 
-    def translate(self, vector):
-        """Translate entity along vector."""
-        logger.debug("Probe.translate: {}".format(vector))
-
-        if not isinstance(vector, np.ndarray):
-            vector = np.array(vector)
-
-        if vector.size < 2:
-            vec = self.normal * np.asscalar(vector)
-            vector = vec._x
-
-        self.p1.translate(vector)
-        self.p2.translate(vector)
-
-    def record(self):
-        self.history.append(self.list)
-
     def distance(self, other):
         """Return the closest distance between entities."""
         dx = super(Probe, self).distance(other)
         return dx - self.size / 2
 
-    def rotate(self, theta, point=None, axis=None):
-        """Rotate entity around an axis which passes through an point by theta
-        radians."""
-        logger.debug("Probe.rotate: {}, {}, {}".format(theta, point, axis))
-        self.p1.rotate(theta, point, axis)
-        self.p2.rotate(theta, point, axis)
-
-        if point is None:
-            d = 0
-        else:
-            d = point._x
-
-    def measure(self, phantom, perc=0.0, pool=None):
+    def measure(self, phantom, theta, h, v, perc=0.0):
         """Return the probe measurement with optional Gaussian noise.
 
         Parameters
@@ -180,13 +139,25 @@ class Probe(Line):
         perc : float >= 0
             Percentage of Gaussian noise.
         """
-        newdata = self.intensity * np.exp(-self._get_attenuation(phantom))
-
-        if perc > 0:
-            newdata += newdata * perc * np.random.normal(scale=1)
-
-        self.record()
-
+        # Convert probe coordinates to cartesian coordinates
+        assert theta.size == h.size == v.size, "theta, h, v must be the" \
+            "equal lengths"
+        newdata = np.zeros(theta.size)
+        z = v
+        cos_p = np.cos(theta)
+        sin_p = np.sin(theta)
+        srcx = +RADIUS*cos_p - h*sin_p
+        srcy = +RADIUS*sin_p + h*cos_p
+        detx = -RADIUS*cos_p - h*sin_p
+        dety = -RADIUS*sin_p + h*cos_p
+        # Calculate measurement for each position
+        for i in range(theta.size):
+            self.p1 = Point([srcx[i], srcy[i]])
+            self.p2 = Point([detx[i], dety[i]])
+            newdata[i] = (self.intensity
+                          * np.exp(-self._get_attenuation(phantom)))
+            if perc > 0:
+                newdata[i] += newdata[i] * perc * np.random.normal(scale=1)
         logger.debug("Probe.measure: {}".format(newdata))
         return newdata
 
@@ -210,10 +181,9 @@ class Probe(Line):
 
     @cached_property
     def cross_section(self):
-        if self.dim == 2:
-            return self.size
-        else:
-            return np.pi * self.size**2 / 4
+        "Return the cross-sectional area of a square beam"
+        return self.size**2
+        # return np.pi * self.size**2 / 4
 
 
 def beamintersect(beam, geometry):
@@ -249,6 +219,7 @@ def beammesh(beam, mesh):
 
 def beampoly(beam, poly):
     """Intersection area of an infinite beam with a polygon"""
+    return 0
     if beam.distance(poly.center) > poly.radius:
         logger.debug("BEAMPOLY: skipped because of radius.")
         return 0
@@ -301,140 +272,6 @@ def beamcirc(beam, circle):
     return a
 
 
-def probe_wrapper(probes, phantom, **kwargs):
-    """Wrap probe.measure to make it suitable for multiprocessing.
-
-    This method does two things: (1) it puts the Probe.measure method in the
-    f(*args) format for the pool workers (2) it passes chunks of work (multiple
-    probes) to the workers to reduce overhead.
-    """
-    measurements = [None] * len(probes)
-    for i in range(len(probes)):
-        measurements[i] = probes[i].measure(phantom, **kwargs)
-
-    return measurements
-
-
-def calculate_gram(procedure, niter, phantom, pool=None,
-                   chunksize=1, mkwargs={}):
-    """Measure the `phantom` using the `procedure`.
-
-    Parameters
-    ----------
-    procedure : :py:`.iterator`
-        An iterator that yields :class:`.Probe`
-    niter : int
-        The number of measurements to take
-    phantom : :class:`.phantom.Phantom`
-    pool : :py:`.multiprocessing.Pool` (default: None)
-    chunksize : int (default: 1)
-        The number of measurements to send to each worker in the pool.
-    mkwargs : dict
-        keyword arguments to pass to :ref:`.Probe.measure`.
-
-    Raise
-    -----
-    ValueError
-        If niter is not a multiple of chunksize
-
-    .. seealso::
-        :class:`.sinogram`, :class:`,angelogram`
-    """
-    measurements = np.zeros(niter)
-
-    if pool is None:  # no multiprocessing
-        logging.info("calculate_gram: {}, single "
-                     "thread".format(procedure.__name__))
-
-        for m in range(niter):
-            probe = next(procedure)
-            measurements[m] = probe.measure(phantom, **mkwargs)
-
-    else:
-        if chunksize == 1:
-            warnings.warn("Default chunksize is 1. This is not optimal.",
-                          RuntimeWarning)
-
-        nchunks = niter // chunksize
-        measurements.shape = (nchunks, chunksize)
-        logging.info("calculate_gram: {}, {} "
-                     "chunks".format(procedure.__name__, nchunks))
-
-        # assign work to pool
-        nmaxqueue = pool._processes * 2
-        async_data = queue.Queue(nmaxqueue)
-        for m in range(nchunks):
-            while async_data.full():
-                item = async_data.get()
-
-                if item[1].ready():
-                    if item[1].successful():
-                        measurements[item[0], :] = item[1].get()
-                    else:
-                        raise RuntimeError('Process Failed '
-                                           'at chunk {}'.format(item[0]))
-                else:  # not ready
-                    async_data.put(item)
-                    time.sleep(5)
-
-            probes = [None] * chunksize
-
-            for n in range(chunksize):
-                probe = next(procedure)
-                probe.record()
-                probe_copy = deepcopy(probe)
-                probe_copy.history = []
-                probes[n] = probe_copy
-
-            async_data.put((m,
-                            pool.apply_async(probe_wrapper,
-                                             (probes, phantom),
-                                             mkwargs))
-                           )
-            logging.info('calculate_gram: chunk {} queued'.format(m))
-
-        while not async_data.empty():
-            item = async_data.get()
-            item[1].wait()
-            if item[1].successful():
-                measurements[item[0], :] = item[1].get()
-            else:
-                raise RuntimeError('Process Failed '
-                                   'at chunk {}'.format(item[0]))
-
-        measurements = measurements.flatten()
-
-    return measurements, probe
-
-
-def sinogram(sx, sy, phantom, pool=None, mkwargs={}):
-    """Return a sinogram of phantom and the probe.
-
-    Parameters
-    ----------
-    sx : int
-        Number of rotation angles.
-    sy : int
-        Number of detection pixels (or sample translations).
-    phantom : Phantom
-
-    Returns
-    -------
-    sino : ndarray
-        Sinogram.
-    probe : Probe
-        Probe with history.
-    """
-    scan = raster_scan(sx, sy)
-
-    sino, probe = calculate_gram(scan, sx*sy, phantom,
-                                 pool=pool, chunksize=sy, mkwargs=mkwargs)
-
-    sino.shape = (sx, sy)
-
-    return sino, probe
-
-
 def raster_scan3D(sz, sa, st, zstart=None):
     """A Probe iterator for raster-scanning in 3D.
 
@@ -453,95 +290,30 @@ def raster_scan3D(sz, sa, st, zstart=None):
     ------
     p : Probe
     """
-    # Step sizes of the probe.
-    tstep = Point([0, 1. / st, 0])
-    zstep = Point([0, 0, 1. / sz])
-    theta = np.pi / sa
-
-    # Fixed probe location.
-    if zstart is None:
-        zstart = 1. / sz / 2.
-
-    p = Probe(Point([-10, 1. / st / 2., zstart]),
-              Point([10, 1. / st / 2., zstart]),
-              size=1. / st)
-
-    for o in range(sz):
-        for m in range(sa):
-            for n in range(st):
-                yield p
-                p.translate(tstep._x)
-            p.translate(-st * tstep._x)
-            p.rotate(theta, Point([0.5, 0.5, 0]))
-            tstep.rotate(theta)
-        p.rotate(np.pi, Point([0.5, 0.5, 0]))
-        tstep.rotate(np.pi)
-        p.translate(zstep._x)
+    raise NotImplementedError
 
 
-def raster_scan(sa, st, width_fraction=1, nmeta=1, random=False, plot=False):
-    """A :py:class:`.Probe` iterator for raster scanning.
-
-    By default, the Probe position is the center of each translation step and
-    there is no gap between steps. `width_fraction` < 1 creates a gap between
-    Probes; `width_fraction` > 1 causes Probes to overlap. With `nmeta` > 1,
-    the starting Probe position will offset after each rotation. When `nmeta`
-    < 1, then `nmeta` will be auto calculately to completely cover the space.
-
+def raster_scan2D(sa, st, meta=False):
+    """
     Parameters
     ----------
     sa : int
         The number of projeciton angles in `[0, 2PI)`.
     st : int
-        The number of Probe steps at each projection angle.
-    width_fraction : float
-        The width of the Probe as a fraction of the step size (`1 / st`).
+        The number of Probe steps at each projection angle. `[-0.5, 0.5)`
     nmeta : int >= 0
         The number of meta steps. Meta steps are the offset from the starting
         Probe position after each rotation.
-    random : bool
-        Whether the meta steps are organized in a random fashion or not.
-    plot : bool
-        Plot a angle vs position plot of the Probe positions.
 
-    Yields
-    ------
-    p : :class:`.Probe`
+    Returns
+    -------
+    theta, h, v : :py:class:`np.array` (M,)
+        Probe positions for scan
     """
-    step = 1. / st
-    width = step * width_fraction
-
-    if nmeta < 1:
-        nmeta = int(np.ceil(step / width))
-    meta_step = np.linspace(0, step, nmeta, endpoint=False)
-    offset = step / nmeta / 2
-
-    theta = 2 * np.pi / sa
-
-    p = Probe(Point([offset, -10]), Point([offset, 10]), width)
-
-    angles = list()
-    positions = list()
-    for m in range(sa):
-        if random:
-            np.random.shuffle(meta_step)
-        p.translate(meta_step[m % nmeta])
-
-        for n in range(st):
-            angles.append(m * theta / np.pi)
-            positions.append(offset + meta_step[m % nmeta] + n*step)
-            yield p
-            p.translate(step)
-
-        p.translate(-1 - meta_step[m % nmeta])
-
-        p.rotate(theta, Point([0.5, 0.5]))
-
-    if plot:
-        import matplotlib.pyplot as plt
-        axis = plt.gca()
-        axis.scatter(positions, angles)
-        plt.xlabel('position [cm]')
-        plt.ylabel('angle [pi rad]')
-        plt.title('raster_scan({}, {}, width_fraction={},\nnmeta={},'
-                  ' random={})'.format(sa, st, width_fraction, nmeta, random))
+    theta = np.linspace(0, np.pi*2, sa, endpoint=False)
+    h = np.linspace(0, 1, st, endpoint=False) - 0.5
+    theta, h = np.meshgrid(theta, h)
+    theta = theta.flatten()
+    h = h.flatten()
+    v = np.zeros(h.shape)
+    return theta, h, v
