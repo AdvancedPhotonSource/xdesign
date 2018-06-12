BUG: Fix improper scaling issues with reconstructions
Reconstruction magntiudes were scaled improperly because algorithms
were slightly off and because the prove measurement model was
dividing by a 2D cross section instead of a 1D cross section

Fixes #52

xdesign/acquisition.py
@@ -176,7 +176,7 @@ class Probe(Line):
     @cached_property
     def cross_section(self):
         """Return the cross-sectional area of a square beam"""
-        return self.size**2
+        return self.size
         # return np.pi * self.size**2 / 4
 
     def half_space(self):

xdesign/algorithms.py
@@ -202,7 +202,7 @@ def sirt(gmin, gsize, data, theta, h, v, init, niter=10):
     for n in range(niter):
 
         update = np.zeros(init.shape)
-        sumdist = np.zeros(init.shape)
+        nupdate = np.zeros(init.shape, dtype=np.uint)
 
         update_progress(n/niter)
         for m in range(data.size):
@@ -224,9 +224,10 @@ def sirt(gmin, gsize, data, theta, h, v, init, niter=10):
                 sim = np.dot(dist[ind], init[ix[ind], iy[ind]])
                 upd = np.true_divide((data[m] - sim), dist2)
                 update[ix[ind], iy[ind]] += dist[ind] * upd
-                sumdist[ix[ind], iy[ind]] += dist[ind]
+                nupdate[ix[ind], iy[ind]] += 1
 
-        init += np.true_divide(update, sumdist * sy)
+        nupdate[nupdate == 0] = 1
+        init += np.true_divide(update, nupdate)
     update_progress(1)
     return init
 