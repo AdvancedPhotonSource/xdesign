d052f4c50a05e08aaaffb6e1be2aacbd50a17aa6
Daniel Ching <carterbox@users.noreply.github.com>
2019-02-27 13:50:18 -0600

    API: Break phantom into submodules

---
+++ src/xdesign/phantom/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Objects and methods for computing the quality of reconstructions.
+
+.. moduleauthor:: Daniel J Ching <carterbox@users.noreply.github.com>
+"""
+
+from xdesign.phantom.custom import *
+from xdesign.phantom.phantom import *
+from xdesign.phantom.standards import *
